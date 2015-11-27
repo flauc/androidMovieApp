d3f6006ce13f45999ef4651d2d90007bef945114
Filip Lauc <filip.lauc93@gmail.com>
2015-11-27 20:58:46 +0100

    Created tab layout in MainActivity

--- app/src/main/java/com/example/detectives/movieapp/MainActivity.java
+++ app/src/main/java/com/example/detectives/movieapp/MainActivity.java
@@ -4,13 +4,26 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +31,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        viewPager = (ViewPager) findViewById(R.id.home_view_pager);
+        setPage(viewPager);
+
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+        setTabIcons();
     }
 
     @Override
@@ -42,5 +62,61 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    // From http://www.androidhive.info/2015/09/android-material-design-working-with-tabs/
+    private void setPage(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new MainActivityFragmentPopularity(), "Popularity");
+        adapter.addFragment(new MainActivityFragmentVotes(), "Rating");
+        adapter.addFragment(new MainActivityFragmentPopularity(), "Favorites");
+        viewPager.setAdapter(adapter);
+    }
+
+        private void setTabIcons() {
+
+            TextView popularTab = (TextView) LayoutInflater.from(this).inflate(R.layout.single_tab, null);
+            popularTab.setText("Popularity");
+            popularTab.setCompoundDrawablesWithIntrinsicBounds(0, R.mipmap.ic_tab_popularity, 0, 0);
+            tabLayout.getTabAt(0).setCustomView(popularTab);
+
+            TextView votesTab = (TextView) LayoutInflater.from(this).inflate(R.layout.single_tab, null);
+            votesTab.setText("Rating");
+            votesTab.setCompoundDrawablesWithIntrinsicBounds(0, R.mipmap.ic_tab_votes, 0, 0);
+            tabLayout.getTabAt(1).setCustomView(votesTab);
+
+            TextView favoritesTab = (TextView) LayoutInflater.from(this).inflate(R.layout.single_tab, null);
+            favoritesTab.setText("Favorites");
+            favoritesTab.setCompoundDrawablesWithIntrinsicBounds(0, R.mipmap.ic_tab_favorites, 0, 0);
+            tabLayout.getTabAt(2).setCustomView(favoritesTab);
+        }
+
+    class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
     }
 }
